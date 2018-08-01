8bf53c19e64109228e8b8601a982d0741c4fabef
nickbarth <barth.nicholas@gmail.com>
2018-08-01 10:14:16 -0400

    styled up the enemy chat bubbles

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react'
+// Vendor
+import React, { Component } from 'react';
 
-import './style.css'
+// Utils
+import { convertToMessageTimestamp } from '../../utils/datetime.js';
+
+// CSS
+import './style.css';
 
 export default class Message extends Component {
 
@@ -17,16 +22,24 @@ export default class Message extends Component {
 
         return (
             <div className="message__container">
-                <div className="message__image" style={{backgroundImage: `url(imgs/${user.username}.jpg)`}}>
-                </div>
-                <div className="message__info">
-                    <div className="message__username">
+                <div className="message__image" style={{backgroundImage: `url(imgs/${user.username}.jpg)`}} />
+                <div className="message__message-container">
+                    <div className="message__info">
+                        <div className="message__realname">
+                            {user.real_name}
+                        </div>
+                        -
+                        <div className="message__username">
+                            @{user.username}
+                        </div>
+                        <div className="message__timestamp">
+                            {convertToMessageTimestamp(post.ts)}
+                        </div>
                     </div>
-                <div className="message__realname">
-                </div>
-                <div className="message__timestamp">
+                    <div className="message__message">
+                        {post.message}
+                    </div>
                 </div>
             </div>
-        </div>
     )}
 }
--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,6 +1,21 @@
-export function toMessageTimestamp(ts) {
-    // Wouldn't be so half assed in real life
-    const date  = new Date(ts);
+/**
+ *  DO NOT EXAMINE THIS FILE PLEASE DONT LOOK AT ANY OF THIS
+ * @param {ts} unix timestramp 
+ */
 
-    return date.toLocaleTimeString();
+export function convertToMessageTimestamp(ts) {
+    const date  = new Date(ts);
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+
+    function addTrailingZero(time) {
+        const timeString = time.toString();
+        if (timeString.length == 1) {
+            return '0' + timeString;
+        } else {
+            return timeString;
+        }
+    }
+
+    return hours + ':' + addTrailingZero(minutes);
 }
